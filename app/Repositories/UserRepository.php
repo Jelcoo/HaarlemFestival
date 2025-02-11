32328b7f0293946f27b93ff8b145814118925ffc
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-02-11 21:22:53 +0000

    Run phpcs:fix

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Repositories;
 
-use App\Models\User;
 use App\Helpers\QueryBuilder;
 
 class UserRepository extends Repository
--- routes/public.php
+++ routes/public.php
@@ -7,5 +7,5 @@ $router = App\Application\Router::getInstance();
 $router->get('/', [App\Controllers\HomeController::class, 'index']);
 
 $router->middleware(EnsureLoggedIn::class, function () use ($router) {
-    // $router->get('/account', [App\Controllers\AccountController::class, 'index']);
+    $router->get('/account', [App\Controllers\HomeController::class, 'account']);
 });
