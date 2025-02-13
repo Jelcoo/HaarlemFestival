ea3613f60f75a9ccaf41c64ca9e240ab744069e5
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-02-13 20:55:43 +0000

    Prevent users from accessing register when logged in

--- routes/public.php
+++ routes/public.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Middleware\EnsureLoggedIn;
+use App\Middleware\EnsureNotLoggedIn;
 
 $router = App\Application\Router::getInstance();
 
 $router->get('/', [App\Controllers\HomeController::class, 'index']);
 
-$router->get('/register', [App\Controllers\AuthController::class, 'register']);
-$router->post('/register', [App\Controllers\AuthController::class, 'registerPost']);
+
+$router->middleware(EnsureNotLoggedIn::class, function () use ($router) {
+    $router->get('/register', [App\Controllers\AuthController::class, 'register']);
+    $router->post('/register', [App\Controllers\AuthController::class, 'registerPost']);
+});
 
 $router->middleware(EnsureLoggedIn::class, function () use ($router) {
     $router->get('/account', [App\Controllers\HomeController::class, 'account']);
