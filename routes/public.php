7979d3581447fae2d6af4547f73c24a2862628f5
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-02-14 11:29:20 +0000

    Status page after completed payment

--- routes/public.php
+++ routes/public.php
@@ -8,6 +8,7 @@ $router->get('/', [App\Controllers\HomeController::class, 'index']);
 $router->get('/cart', [App\Controllers\StripeController::class, 'cart']);
 $router->get('/checkout', [App\Controllers\StripeController::class, 'index']);
 $router->post('/checkout/create', [App\Controllers\StripeController::class, 'create']);
+$router->get('/checkout/complete', [App\Controllers\StripeController::class, 'complete']);
 
 $router->middleware(EnsureLoggedIn::class, function () use ($router) {
     $router->get('/account', [App\Controllers\HomeController::class, 'account']);
