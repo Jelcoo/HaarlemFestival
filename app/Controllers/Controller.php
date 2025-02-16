b08c02dedc67ab38ecf909500c8cfde8f245efbc
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-02-16 17:49:34 +0000

    Add getAuthUser to controller

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -3,13 +3,32 @@
 namespace App\Controllers;
 
 use App\Application\PageLoader;
+use App\Application\Session;
+use App\Models\User;
+use App\Repositories\UserRepository;
 
 class Controller
 {
     protected PageLoader $pageLoader;
+    private UserRepository $userRepository;
+    private User $user;
 
     public function __construct()
     {
         $this->pageLoader = new PageLoader();
+        $this->userRepository = new UserRepository();
+    }
+
+    protected function getAuthUser(): ?User
+    {
+        if (isset($this->user)) {
+            return $this->user;
+        }
+        
+        if (Session::isValidSession()) {
+            $this->user = $this->userRepository->getUserById($_SESSION['user_id']);
+        }
+
+        return $this->user;
     }
 }
