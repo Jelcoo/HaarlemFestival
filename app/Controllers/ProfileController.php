94425808d3e47d8872805dab1a476ee20449315b
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-02-24 21:37:37 +0000

    input validation

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Validation\UniqueRule;
+use Rakit\Validation\Validator;
 use App\Repositories\UserRepository;
 
 class ProfileController extends Controller
@@ -14,26 +16,67 @@ class ProfileController extends Controller
         $this->userRepository = new UserRepository();
     }
 
-    public function index()
+    public function index(): string
     {
         $user = $this->userRepository->getUserById($_SESSION['user_id']);
 
         return $this->pageLoader->setPage('account/manage')->render(['user' => $user]);
     }
 
-    public function update()
+    public function update(): string
     {
-        // die(var_dump($_POST));
-        $user = $this->userRepository->getUserById($_SESSION['user_id']);
-        $user->firstname = $_POST['firstname'] ?? $user->firstname;
-        $user->lastname = $_POST['lastname'] ?? $user->lastname;
-        $user->email = $_POST['email'] ?? $user->email;
-        $user->address = $_POST['address'] ?: $user->address;
-        $user->city = $_POST['city'] ?: $user->city;
-        $user->postal_code = $_POST['postal_code'] ?: $user->postal_code;
+        try {
+            $user = $this->userRepository->getUserById($_SESSION['user_id']);
+        } catch (\Exception $e) {
+            return $this->rerender([
+                'error' => $e->getMessage(),
+                'fields' => $_POST,
+            ]);
+        }
 
-        $this->userRepository->updateUser($user);
+        $validator = new Validator();
+        $validator->addValidator('unique', new UniqueRule());
+
+        $rules = [
+            'firstname' => 'required|max:255',
+            'lastname' => 'required|max:255',
+            'address' => 'max:255',
+            'city' => 'max:255',
+            'postal_code' => 'max:255',
+        ];
+        if ($user->email !== $_POST['email']) {
+            $rules['email'] = 'required|email|unique:users,email|max:255';
+        }
+
+        $validation = $validator->validate($_POST, $rules);
+
+        if ($validation->fails()) {
+            return $this->rerender([
+                'error' => $validation->errors()->toArray(),
+                'fields' => $_POST,
+            ]);
+        }
+        try {
+            $user->firstname = $_POST['firstname'] ?? $user->firstname;
+            $user->lastname = $_POST['lastname'] ?? $user->lastname;
+            $user->email = $_POST['email'] ?? $user->email;
+            $user->address = $_POST['address'] ?: $user->address;
+            $user->city = $_POST['city'] ?: $user->city;
+            $user->postal_code = $_POST['postal_code'] ?: $user->postal_code;
+
+            $this->userRepository->updateUser($user);
+        } catch (\Exception $e) {
+            return $this->rerender([
+                'error' => $e->getMessage(),
+                'fields' => $_POST,
+            ]);
+        }
 
         return $this->pageLoader->setPage('account/manage')->render(['user' => $user]);
+    }
+
+    private function rerender(array $parameters = []): string
+    {
+        return $this->pageLoader->setPage('account/manage')->render($parameters);
     }
 }
