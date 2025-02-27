ba7d2fe680b37908981d88f332c6805386cb3c63
Daan <102171846+gaxzy@users.noreply.github.com>
2025-02-27 10:07:54 +0100

    Fix sidebar height

--- resources/views/components/dashboard/sidebar.php
+++ resources/views/components/dashboard/sidebar.php
@@ -1,4 +1,4 @@
-<nav class="col-lg-2 bg-dark text-white vh-100 p-3">
+<nav class="col-lg-2 bg-dark text-white min-vh-100 p-3">
     <h4 class="text-center">Dashboard</h4>
     <ul class="nav flex-column">
         <?php foreach ($sidebarItems as $key => $item) { ?>
