8c3d30bb75d9a76b94e1d113cd60609603d98073
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-03-03 17:09:26 +0100

    Convert newlines to <br>

--- resources/views/components/editor.php
+++ resources/views/components/editor.php
@@ -8,9 +8,14 @@
         selector: '#editor',
         license_key: 'gpl',
         promotion: false,
+
         height: 400,
+
         menubar: true,
         plugins: 'anchor autolink charmap codesample emoticons image link lists media preview searchreplace table visualblocks wordcount',
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+
+        forced_root_block: false,
+        br_newlines: true,
     });
 </script>
