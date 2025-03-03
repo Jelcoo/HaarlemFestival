c5e22ae030a423f92bdb5964470a94a1d06cc19c
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-03-03 16:18:29 +0100

    Add editor component

---
+++ resources/views/components/editor.php
@@ -0,0 +1,16 @@
+<script src="https://cdn.jsdelivr.net/npm/tinymce@7.7.0/tinymce.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tinymce@7.7.0/skins/ui/oxide/content.min.css">
+
+<textarea id="editor"></textarea>
+
+<script>
+    tinymce.init({
+        selector: '#editor',
+        license_key: 'gpl',
+        promotion: false,
+        height: 400,
+        menubar: true,
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media preview searchreplace table visualblocks wordcount',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+    });
+</script>
