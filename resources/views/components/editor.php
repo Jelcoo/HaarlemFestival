3d754647ecae3d9bfa9bf4b28e085ce7178eb1a9
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-03-03 17:17:53 +0100

    Replace deprecated behaviour

--- resources/views/components/editor.php
+++ resources/views/components/editor.php
@@ -15,7 +15,6 @@
         plugins: 'anchor autolink charmap codesample emoticons image link lists media preview searchreplace table visualblocks wordcount',
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
 
-        forced_root_block: false,
-        br_newlines: true,
+        newline_behavior: 'linebreak',
     });
 </script>
