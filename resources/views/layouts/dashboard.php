7ddeb1bbe93cae5ec4d88ca9577dbe89f9f3f9cd
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-03-05 11:03:01 +0100

    Fix dashboard layout

---
+++ resources/views/layouts/dashboard.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Application\Session;
+
+$loggedIn = Session::isValidSession();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico">
+
+    <title>Dashboard - The Festival</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script>
+        window.addEventListener("DOMContentLoaded", () => {
+            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+        });
+    </script>
+
+    <script src="/assets/js/utils.js"></script>
+</head>
+
+<body>
+    {{content}}
+    <?php
+        if (App\Config\Config::getKey('APP_ENV') === 'development') {
+            $endtime = microtime(true);
+            printf('Page loaded in %f seconds', $endtime - $GLOBALS['APP_START_TIME']);
+        } ?>
+</body>
+
+</html>
