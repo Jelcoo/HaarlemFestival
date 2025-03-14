f3e93ee26a769b0bef06d6dcc433df535ebcf6e9
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-03-14 15:23:19 +0100

    Fix stripe erroring without config key

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -4,10 +4,10 @@ namespace App\Config;
 
 class Config
 {
-    public static function getKey(string $key): mixed
+    public static function getKey(string $key, mixed $default = null): mixed
     {
         $config = require __DIR__ . '/../../config.php';
 
-        return $config[$key] ?? null;
+        return $config[$key] ?? $default;
     }
 }
