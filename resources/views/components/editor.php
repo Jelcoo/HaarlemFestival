cf1ca8d726c61b9cf4f6906f364b4e147e0570b9
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-03-14 14:32:19 +0100

    Get rid of custom image uploading

--- resources/views/components/editor.php
+++ resources/views/components/editor.php
@@ -16,25 +16,5 @@
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
 
         newline_behavior: 'linebreak',
-        
-        images_upload_handler: (blobInfo, progress) => new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.withCredentials = false;
-            xhr.open('POST', '/upload');
-            xhr.upload.onprogress = (e) => {
-                progress(e.loaded / e.total);
-            };
-            xhr.onload = () => {
-                const json = JSON.parse(xhr.responseText);
-                if (xhr.status < 200 || xhr.status >= 300) {
-                    reject({ message: json.error, remove: true });
-                    return;
-                }
-                resolve(json.location);
-            };
-            const formData = new FormData();
-            formData.append('file', blobInfo.blob(), blobInfo.filename());
-            xhr.send(formData);
-        }),
     });
 </script>
