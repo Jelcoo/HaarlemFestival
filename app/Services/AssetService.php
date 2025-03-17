ab33b9cf17df9befe6038d2ebeb3ab89d422d275
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-03-17 15:38:18 +0100

    Add note to AssetService

--- app/Services/AssetService.php
+++ app/Services/AssetService.php
@@ -5,6 +5,11 @@ namespace App\Services;
 use App\Models\Asset;
 use App\Repositories\AssetRepository;
 
+/**
+ * You can learn how to use this class in the file app/Controllers/DashboardArtistsController.php.
+ * Search for "$this->assetService" to see where it is used.
+ */
+
 class AssetService
 {
     private AssetRepository $assetRepository;
